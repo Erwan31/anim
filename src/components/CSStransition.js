b0ddc3c65f4b2946215a91a7277546c42aa3b92f
Erwan31 <spilmonterwan@gmail.com>
2020-08-08 15:15:12 +0100

    Transition group

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-
 import '../css/App.css';
+import { CSSTransition } from 'react-transition-group';
 
 class Fade extends Component{
+
+    state = {
+        show: true,
+    }
+
+    showDiv = () => {
+        this.setState( { show: !this.state.show});
+    }
+
     render(){
+
+        
         return(
-            <div>
-                CSS TRANSITIONS
-            </div>
+            <>
+                <CSSTransition
+                in={this.state.show}
+                timeout={5000}
+                classNames="square">
+                    <div 
+                        className={`square ${this.state.show}`}>
+                        Hello
+                    </div>
+                </CSSTransition>
+
+                <div 
+                className="showDiv"
+                onClick={this.showDiv}>
+                    CSS TRANSITIONS
+                </div>
+            </>
         )
     }
 }
--- src/components/Tgroup.js
+++ src/components/Tgroup.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Slide extends Component{
     state = {
-        items:[]
+        items:['24']
     }
     
     addElements () {
-      
+      return this.state.items.map( (item, i)=> (
+          <CSSTransition
+            classNames="item"
+            timeout={500}
+            key={i}
+            onEntered={ (node)=>{
+                node.classList.add("active");
+            }}
+            onExited={ (node)=>{
+                node.classList.remove("active");
+            }}
+          >
+              <div className="item"  className="item">{item}</div>
+          </CSSTransition>
+          
+      ))
     }
 
     generateNumber(){
+        let newArray = [
+            ...this.state.items,
+            Math.floor(Math.random()*100)+1
+        ]
+
+        this.setState({
+            items: newArray
+        });
        
     }
 
     removeNumber(){
-      
+        let newArray = this.state.items.slice(0,-1);
+
+        this.setState({
+            items: newArray
+        });
     }
 
     render(){
         return(
             <div>
-                {this.addElements()}
-             
                 <div className="btns">
                     <div className="btn-add" onClick={()=> this.generateNumber()}>Add Elements</div>
                     <div className="btn-remove" onClick={()=> this.removeNumber()}>Remove Elements</div>
                 </div>
+                <TransitionGroup
+                    component="div"
+                    className="list">
+                    {this.addElements()}
+                </TransitionGroup>
+                
             </div>
         )
     }
